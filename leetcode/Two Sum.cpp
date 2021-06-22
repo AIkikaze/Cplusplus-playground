23ae971222d092a26b40098422db1dc06625161e
Wenwenziy <wenwenziy@163.com>
2021-06-22 19:58:15 +0800

    第一次提交

---
+++ leetcode/Two Sum.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+# include <vector>
+using namespace std;
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//       vector<int> answer;
+//       return answer;
+//     }
+// };
+
+int main() {
+  cout << 8 + 9 << endl;  
+  return 0;
+}
+
