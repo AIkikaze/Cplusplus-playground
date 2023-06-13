dba311e5295cb61dbdc9fbfa705472a1e8626116
AIkikaze <wenwenziy@163.com>
2023-06-13 17:43:26 +0800

    删除多余的 MIPP库

--- Projects_cpp/project_shapeBasedMatch/cpp/line2dup/main.cpp
+++ Projects_cpp/project_shapeBasedMatch/cpp/line2dup/main.cpp
@@ -3,6 +3,30 @@
 using namespace std;
 using namespace cv;
 
+void MIPP_test() {
+  cout << "MIPP tests" << endl;
+  cout << "----------" << endl << endl;
+
+  cout << "Instr. type:       " << mipp::InstructionType << endl;
+  cout << "Instr. full type:  " << mipp::InstructionFullType << endl;
+  cout << "Instr. version:    " << mipp::InstructionVersion << endl;
+  cout << "Instr. size:       " << mipp::RegisterSizeBit << " bits" << endl;
+  cout << "Instr. lanes:      " << mipp::Lanes << endl;
+  cout << "64-bit support:    " << (mipp::Support64Bit ? "yes" : "no") << endl;
+  cout << "Byte/word support: " << (mipp::SupportByteWord ? "yes" : "no")
+       << endl;
+
+#ifndef has_max_int8_t
+  cout << "in this SIMD, int8 max is not inplemented by MIPP" << endl;
+#endif
+
+#ifndef has_shuff_int8_t
+  cout << "in this SIMD, int8 shuff is not inplemented by MIPP" << endl;
+#endif
+
+  cout << "----------" << endl << endl;
+}
+
 class Timer {
 public:
   Timer() : start_(0), time_(0) {}
@@ -25,8 +49,6 @@ public:
 private:
   int64 start_, time_;
 };
-
-
 
 int main() {
   // bool show_match_result = true;
@@ -71,7 +93,6 @@ int main() {
     printf("*** Added template (id %d) for new object class %d ***\n", template_id, num_classes);
   }
   
-  Point p1(bb.x, bb.y);
   Point p2(bb.x + bb.width, bb.y + bb.height);
   rectangle(display, p1, p2, Scalar(0, 0, 0), 3);
   rectangle(display, p1, p2, Scalar(0, 255, 0), 1);
